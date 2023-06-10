c9aa64c1ca4d1a7f05162ff7d184dffceb7afa5e
Serius Ndruru <129859832+rius23@users.noreply.github.com>
2023-06-11 05:01:02 +0700

    Update DDL Script Perusahaan.sql

--- DDL Script Perusahaan.sql
+++ DDL Script Perusahaan.sql
@@ -4,20 +4,20 @@ USE  perusahaan;
 
 	-- Tabel Departemen
 CREATE TABLE departemen (
-	id_departemen INT PRIMARY KEY,
+    id_departemen INT PRIMARY KEY,
     nama_departemen VARCHAR(255)
     );
 
 	-- Table Manager
 CREATE TABLE manager (
-	id_manager INT PRIMARY KEY,
+    id_manager INT PRIMARY KEY,
     nama_manager VARCHAR(255),
     tanggal_Lahir DATE
 	);
 
 	-- Table Karyawan
 CREATE TABLE karyawan (
-	id_karyawan INT PRIMARY KEY,
+    id_karyawan INT PRIMARY KEY,
     nama_karyawan VARCHAR(255),
     tanggal_lahir DATE,
     id_departemen INT,
@@ -26,14 +26,14 @@ CREATE TABLE karyawan (
 
 	-- Table Supervisor
 CREATE TABLE supervisor (
-	id_supervisor INT PRIMARY KEY,
+    id_supervisor INT PRIMARY KEY,
     nama_supervisor VARCHAR(255),
     tanggal_lahir DATE
     );
     
     -- Table Projek
 CREATE TABLE projek (
-	id_projek INT PRIMARY KEY,
+    id_projek INT PRIMARY KEY,
     nama_projek VARCHAR(255),
     lokasi_projek VARCHAR(255),
     id_departemen INT,
